9463e4ff4f15498032ef92668dec952a1796cc38
niiyazobug <niiyaz5555@gmail.com>
2022-07-08 16:20:13 +0600

    hand landmark drow method impliment

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ import mediapipe as mp
 
 cap = cv2.VideoCapture(0)
 hand_detector = mp.solutions.hands.Hands()
+drawing_utils = mp.solutions.drawing_utils
 
 while True:
     _, frame = cap.read()
+    frame = cv2.flip(frame, 1)
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     output = hand_detector.process(rgb_frame)
     hands = output.multi_hand_landmarks
-    print(hands)
+    # print(hands)
+    if hands:
+        for hand in hands:
+            drawing_utils.draw_landmarks(frame, hand)
+
     cv2.imshow('Virtual Mouse', frame)
     cv2.waitKey(1)
