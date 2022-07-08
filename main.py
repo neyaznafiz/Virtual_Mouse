725e847b5ce3cc54fefc9b30ec2c49b17e8b251d
niiyazobug <niiyaz5555@gmail.com>
2022-07-08 16:47:39 +0600

    cursor allow for ful screen method impliment

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import cv2
 import mediapipe as mp
+import  pyautogui
 
 cap = cv2.VideoCapture(0)
 hand_detector = mp.solutions.hands.Hands()
 drawing_utils = mp.solutions.drawing_utils
+screen_width, screen_height = pyautogui.size()
 
 while True:
     _, frame = cap.read()
@@ -23,6 +25,10 @@ while True:
                 # print(x, y)
                 if id == 8:
                     cv2.circle(img=frame, center=(x, y), radius=10, color=(0, 255, 0))
+                    index_x = screen_width / frame_width * x
+                    index_y = screen_height / frame_height * y
+                    pyautogui.moveTo(index_x, index_y)
+
 
     cv2.imshow('Virtual Mouse', frame)
     cv2.waitKey(1)
