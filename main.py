922ea6e869762e583d1dbba9b2d467555ed4b349
niiyazobug <niiyaz5555@gmail.com>
2022-07-08 17:23:33 +0600

    click method impliment

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ hand_detector = mp.solutions.hands.Hands()
 drawing_utils = mp.solutions.drawing_utils
 screen_width, screen_height = pyautogui.size()
 
+index_y = 0
 while True:
     _, frame = cap.read()
     frame = cv2.flip(frame, 1)
@@ -32,11 +33,15 @@ while True:
                     pyautogui.moveTo(index_x, index_y)
 
                 # for click
-                if id == 4:
+                if id == 12:
                     cv2.circle(img=frame, center=(x, y), radius=10, color=(0, 255, 0))
-                    index_x = screen_width / frame_width * x
-                    index_y = screen_height / frame_height * y
-
+                    thumb_x = screen_width / frame_width * x
+                    thumb_y = screen_height / frame_height * y
+                    print('outside', abs(index_y - thumb_y))
+                    if abs(index_y - thumb_y) < 30:
+                        print('click')
+                        pyautogui.click()
+                        pyautogui.sleep(1)
 
     cv2.imshow('Virtual Mouse', frame)
     cv2.waitKey(1)
